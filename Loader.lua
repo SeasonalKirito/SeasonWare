e19985272f8e5a22450a9c9d95310d464db4ad72
Proxy <99521865+SeasonalKirito@users.noreply.github.com>
2022-12-09 13:27:50 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,3 +4,4 @@ elseif game.PlaceId == 9819654737 then --Raise A Sonic--
   loadstring(game:HttpGet('https://raw.githubusercontent.com/SeasonalKirito/SeasonWare/main/SeasonWare/Raise%20A%20Sonic.lua?token=GHSAT0AAAAAABZARLLYKVI7FZ45SKQQXOWQY4TRX5A'))()
 else --SeasonWare Universal--
   loadstring(game:HttpGet('https://raw.githubusercontent.com/SeasonalKirito/SeasonWare/main/SeasonWare/SeasonWare%20Universal.lua?token=GHSAT0AAAAAABZARLLYQ52MHQ2MK2YGL6GEY4TRYQA'))()
+end
