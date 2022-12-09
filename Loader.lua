ae3bf97b1f6d729f14aea33d9ee05a30bd021544
Proxy <99521865+SeasonalKirito@users.noreply.github.com>
2022-12-09 13:32:14 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,10 @@
 if game.PlaceId == 5804394780 then --Raise A Monke--
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/SeasonalKirito/SeasonWare/main/SeasonWare/Raise%20A%20Monkey.lua?token=GHSAT0AAAAAABZARLLZ6WMOO7QQQGRRTNCAY4TRWMQ'))()
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/SeasonalKirito/SeasonWare/main/SeasonWare/Raise%20A%20Monkey.lua?token=GHSAT0AAAAAABZARLLZFTZOQY7H3WH7Q7SWY4TTMQA'))()
   
 elseif game.PlaceId == 9819654737 then --Raise A Sonic--
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/SeasonalKirito/SeasonWare/main/SeasonWare/Raise%20A%20Sonic.lua?token=GHSAT0AAAAAABZARLLYKVI7FZ45SKQQXOWQY4TRX5A'))()
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/SeasonalKirito/SeasonWare/main/SeasonWare/Raise%20A%20Sonic.lua?token=GHSAT0AAAAAABZARLLYS3SB7QFG4TXO3CBCY4TTM4A'))()
+  
+else --SeasonWare Universal--
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/SeasonalKirito/SeasonWare/main/SeasonWare/SeasonWare%20Universal.lua?token=GHSAT0AAAAAABZARLLZ2NL22OXNHASM4TWSY4TTN3A'))()
+  
 end
