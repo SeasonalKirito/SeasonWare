ddee8688a0c2c3d1a9bfd7b1e4d0b0973e5c3946
Proxy <99521865+SeasonalKirito@users.noreply.github.com>
2022-12-10 00:27:19 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,7 +4,7 @@ if game.PlaceId == 5804394780 then --Raise A Monke--
 elseif game.PlaceId == 9819654737 then --Raise A Sonic--
   loadstring(game:HttpGet("https://raw.githubusercontent.com/SeasonalKirito/SeasonWare/main/SeasonWare/Raise%20A%20Sonic.lua"))()
   
-elseif game.PlaceId == 9980743733 them --[☃]Raise a Van 2--
+elseif game.PlaceId == 9980743733 then --[☃]Raise a Van 2--
   loadstring(game:HttpGet("https://raw.githubusercontent.com/SeasonalKirito/SeasonWare/main/SeasonWare/%5B☃%5DRaise%20a%20Van%202.lua"))()
   
 else --SeasonWare Universal--
