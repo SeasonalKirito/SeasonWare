5db02c81303d8cfe005220983789d89351a37ae5
Proxy <99521865+SeasonalKirito@users.noreply.github.com>
2022-12-20 01:47:47 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -10,6 +10,9 @@ elseif game.PlaceId == 9980743733 then --[☃]Raise a Van 2--
 elseif game.PlaceId == 6284583030 then --[🎄 EVENT] Pet Simulator X! 🐾--
   loadstring(game:HttpGet("https://raw.githubusercontent.com/SeasonalKirito/SeasonWare/main/SeasonWare/Pet%20Simulator%20X%20Auto%20Gift.lua"))()
   
+elseif game.PlaceId == 11381074034 then --[🎄Xmas UPD] Flying Race Clicker--
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SeasonalKirito/SeasonWare/main/SeasonWare/%5B🎄Xmas%20UPD%5D%20Flying%20Race%20Clicker.lua"))()
+  
 else --SeasonWare Universal--
   loadstring(game:HttpGet("https://raw.githubusercontent.com/SeasonalKirito/SeasonWare/main/SeasonWare/SeasonWare%20Universal.lua"))()
   
